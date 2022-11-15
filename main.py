432ccefdd5c5a7a55aa258db012336c2ffe399d0
gitkamaal <59745982+gitkamaal@users.noreply.github.com>
2022-11-15 13:17:05 -0500

    INIT

---
+++ main.py
@@ -0,0 +1,43 @@
+# FLASK Tutorial 1 -- We show the bare-bones code to get an app up and running
+
+# imports
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask  # Flask is the web app that we will customize
+from flask import render_template
+from flask import request
+from flask import redirect, url_for 
+from database import db
+
+
+app = Flask(__name__)  # create an app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
+
+# @app.route is a decorator. It gives the function "index" special powers.
+# In this case it makes it so anyone going to "your-url/" makes this function
+# get called. What it returns is what is shown as the web page
+@app.route('/')
+@app.route('/index')
+def index():    
+
+    return render_template('main.html')
+
+
+
+
+
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
+
+# To see the web page in your web browser, go to the url,
+#   http://127.0.0.1:5000/index
+
+# Note that we are running with "debug=True", so if you make changes and save it
+# the server will automatically update. This is great for development but is a
+# security risk for production.
