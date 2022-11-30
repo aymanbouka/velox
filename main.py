7d478467b68076d78838a5d9731fbe66d08a1287
gitkamaal <59745982+gitkamaal@users.noreply.github.com>
2022-11-30 12:25:23 -0500

    Frontend design changes

--- main.py
+++ main.py
@@ -180,6 +180,12 @@ def login():
         # form did not validate or GET request
         return render_template("login.html", form=login_form)
 
+@app.route('/logout')
+def logout():
+    if session.get('user'):
+        session.clear()
+    return redirect(url_for('main'))
+
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
